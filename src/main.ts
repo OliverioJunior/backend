8b0330b7356f3f7e6557c20599cbaf1d4ad97eee
OliverioJunior <oliverio.junior2@gmail.com>
2025-06-03 20:03:35 -0300

    feat: add port configuration and deploy scripts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
